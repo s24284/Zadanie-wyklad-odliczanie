d0ed2e8863e96d7bc4e7aaaf7e0d177fd65bed86
Anna Prymas <s24284@pjwstk.edu.pl>
2021-11-03 18:54:17 +0100

    Dodaj pliki projektów.

---
+++ Zadanie wyklad odliczanie/Zadanie wyklad odliczanie.cpp
@@ -0,0 +1,31 @@
+﻿// Zadanie wyklad odliczanie.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+
+int main()
+{
+    int start;
+    std::cout << "Podaj liczbe calkowita dodatnia, od ktorej rozpoczniemy odliczanie do 0:\n";
+    std::cin >> start;
+
+    if (start < 0)
+        
+    {
+        std::cout << "Podales liczbe mniejsza od zero.";
+    }
+
+    else
+
+     std::cout << "Odliczam od " << start << ":\n";
+
+    for (int i = start; i >= 0; i--)
+       
+    {
+       
+       std::cout << i << "\n";
+    }
+
+   return 0;
+}
+
